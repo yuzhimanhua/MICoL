ec7a1a9bf1a51e7b4acab11fb2c43163eb2d1f87
Yu Zhang <yuz9@illinois.edu>
2023-06-02 11:39:47 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -5,10 +5,10 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 echo "${green}=====Step 1: Preparing Testing Data=====${reset}"
-python prepare_test.py --dataset ${dataset}
+python3 prepare_test.py --dataset ${dataset}
 
 echo "${green}=====Step 2: Generating Training Data=====${reset}"
-python prepare_train.py --dataset ${dataset} --metagraph ${metagraph}
+python3 prepare_train.py --dataset ${dataset} --metagraph ${metagraph}
 
 head -100000 ${dataset}_input/dataset.txt > ${dataset}_input/train.txt
 sed -n '100001,110000p' ${dataset}_input/dataset.txt > ${dataset}_input/dev.txt
